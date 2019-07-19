0c621ffdcf1a5795b9bc3c130fc753c9f6ff2859
Mehran-SP <mehran.safaripour@gmail.com>
2019-07-19 12:51:24 +0430

    recreate virtualenv

--- .env/bin/activate.fish
+++ .env/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/mehransp/workspace/python/prometheus/.env"
+set -gx VIRTUAL_ENV "/home/mehransp/workspace/git/Prometheus/.env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
